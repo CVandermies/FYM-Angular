55cfd0f16cd4265bfc269f21a66775ef50dc65b2
CVandermies <charlesvdm@outlook.com>
2021-05-29 06:56:01 +0200

    add bootswatch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap'; 
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
